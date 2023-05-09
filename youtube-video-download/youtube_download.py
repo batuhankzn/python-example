faa937d6698e784e88060a75561ff1d4039ffb45
Batuhan Kazan <batuhankzn61@gmail.com>
2023-05-09 14:54:31 +0300

    pytube

--- youtube-video-download/youtube_download.py
+++ youtube-video-download/youtube_download.py
@@ -1,7 +1,10 @@
 import pytube
 
+#kullanıcıdan video linki
 url = input("enter video url: ")
 
+#boş bırakıyoruz çıktıyı dizine gönderecek
 path = ""
 
+#pytube kütüphanesi
 pytube.YouTube(url).streams.get_highest_resolution().download(path)
