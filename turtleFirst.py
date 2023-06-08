228e27a13a59bbfecc6bc4a35d8e3402b9ae4e06
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-08 16:35:44 +0300

    polygon

--- turtleFirst.py
+++ turtleFirst.py
@@ -6,11 +6,24 @@ board_title = turtle.title("First Turtle Example")
 
 
 #square
-
+'''
 turtle1 = turtle.Turtle()
 for i in range(4):
     turtle1.forward(100)
     turtle1.left(90)
+'''
+
+#polygon
+
+turtle2 = turtle.Turtle()
+
+num_sides = 7
+angle = 360 / num_sides
+side_lenght = 100
+
+for i in range(num_sides):
+    turtle2.right(angle)
+    turtle2.forward(side_lenght)
 
 
 
