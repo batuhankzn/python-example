f0c05a4a2754e2f5b44f2dc22fe233759898862e
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-08 16:27:18 +0300

    square

---
+++ turtleFirst.py
@@ -0,0 +1,17 @@
+import turtle
+
+board = turtle.Screen()
+bg_color = turtle.bgcolor("#75E6DA")
+board_title = turtle.title("First Turtle Example")
+
+
+#square
+
+turtle1 = turtle.Turtle()
+for i in range(4):
+    turtle1.forward(100)
+    turtle1.left(90)
+
+
+
+turtle.done()
