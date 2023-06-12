a962970a44107689e5129a28c702a51029fdd8e9
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-12 12:38:08 +0300

    make turtles and board settings

--- catch-the-turtle.py
+++ catch-the-turtle.py
@@ -6,6 +6,20 @@ board_title = turtle.title("Catch The Turtle")
 FONT = ("Arial", 34, "normal")
 grid_size = 10
 
+#game board
+gameBoard = turtle.Turtle()
+gameBoard.shapesize(3,3)
+gameBoard.hideturtle()
+gameBoard.penup()
+gameBoard.goto(-450,450)
+gameBoard.pendown()
+gameBoard.goto(450,450)
+gameBoard.goto(450,-450)
+gameBoard.goto(-450,-450)
+gameBoard.goto(-450,450)
+
+
+
 #score
 score = turtle.Turtle()
 
@@ -16,22 +30,57 @@ def setup_score_turtle():
     score.write(arg="Score: 0", move=False, align="center", font=FONT)
 
 
-
+#make turtle
 def make_turtle(x,y):
     t = turtle.Turtle()
+    t.speed(0)
     t.penup()
     t.shape("turtle")
     t.shapesize(3,3)
     t.color("dark green")
     t.goto(x * grid_size, y * grid_size )
     
-make_turtle(10,10)
-make_turtle(20,20)
-make_turtle(30,30)
-make_turtle(40,40)
 
-make_turtle(-10,-10)
-make_turtle(-20,-20)
+# 1 and 2 area
+make_turtle(0,0)
+make_turtle(25,0)
+make_turtle(40,0)
+make_turtle(0,35)
+make_turtle(0,20)
+make_turtle(-25,0)
+make_turtle(-40,0)
+
+#1
+make_turtle(30,27)
+make_turtle(33,19)
+make_turtle(15,13)
+make_turtle(15,32)
+
+#2
+make_turtle(-30,27)
+make_turtle(-33,19)
+make_turtle(-15,13)
+make_turtle(-15,32)
+
+
+# 3 and 4 area
+make_turtle(0,-22)
+
+#3
+make_turtle(-30,-27)
+make_turtle(-33,-19)
+make_turtle(-15,-13)
+make_turtle(-15,-32)
+
+#4
+make_turtle(30,-27)
+make_turtle(33,-19)
+make_turtle(15,-13)
+make_turtle(15,-32)
+
+
+
+
 
 
 setup_score_turtle()
