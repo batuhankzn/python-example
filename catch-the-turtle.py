0b28b94f9564533d0333381c1ce251d5e730062f
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-12 18:53:58 +0300

    turtles make changed

--- catch-the-turtle.py
+++ catch-the-turtle.py
@@ -1,26 +1,30 @@
 import turtle
+import random
 
 board = turtle.Screen()
 bg_color = turtle.bgcolor("gray")
 board_title = turtle.title("Catch The Turtle")
 FONT = ("Arial", 34, "normal")
 grid_size = 10
+scoreB = 0
+
 
 #game board
-gameBoard = turtle.Turtle()
-gameBoard.shapesize(3,3)
-gameBoard.hideturtle()
-gameBoard.penup()
-gameBoard.goto(-450,450)
-gameBoard.pendown()
-gameBoard.goto(450,450)
-gameBoard.goto(450,-450)
-gameBoard.goto(-450,-450)
-gameBoard.goto(-450,450)
+def gameBoard_setup():
+    gameBoard = turtle.Turtle()
+    gameBoard.shapesize(3,3)
+    gameBoard.hideturtle()
+    gameBoard.penup()
+    gameBoard.goto(-450,450)
+    gameBoard.pendown()
+    gameBoard.goto(450,450)
+    gameBoard.goto(450,-450)
+    gameBoard.goto(-450,-450)
+    gameBoard.goto(-450,450)
 
 
 
-#score
+#score board
 score = turtle.Turtle()
 
 def setup_score_turtle():
@@ -33,56 +37,59 @@ def setup_score_turtle():
 #make turtle
 def make_turtle(x,y):
     t = turtle.Turtle()
+
+    def handle_click(x,y):
+        global scoreB
+        scoreB += 1
+        score.clear()
+        score.write(arg=f"Score: {scoreB}", move=False, align="center", font=FONT)
+
+    t.onclick(handle_click)
     t.speed(0)
     t.penup()
     t.shape("turtle")
     t.shapesize(3,3)
     t.color("dark green")
     t.goto(x * grid_size, y * grid_size )
+    turtle_list.append(t)
     
 
-# 1 and 2 area
-make_turtle(0,0)
-make_turtle(25,0)
-make_turtle(40,0)
-make_turtle(0,35)
-make_turtle(0,20)
-make_turtle(-25,0)
-make_turtle(-40,0)
+def setup_turtles():
+    x_coordinates = [-30,-20,-10,0,10,20,30]
+    y_coordinates = [30,20,10,0,-10,-20,-30]
 
-#1
-make_turtle(30,27)
-make_turtle(33,19)
-make_turtle(15,13)
-make_turtle(15,32)
-
-#2
-make_turtle(-30,27)
-make_turtle(-33,19)
-make_turtle(-15,13)
-make_turtle(-15,32)
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x,y)
+    
 
 
-# 3 and 4 area
-make_turtle(0,-22)
+#turtles hide            
+turtle_list = []
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
 
-#3
-make_turtle(-30,-27)
-make_turtle(-33,-19)
-make_turtle(-15,-13)
-make_turtle(-15,-32)
-
-#4
-make_turtle(30,-27)
-make_turtle(33,-19)
-make_turtle(15,-13)
-make_turtle(15,-32)
+#show only 1 turtle
+def show_turtles():
+    hide_turtles()
+    random.choice(turtle_list).showturtle()
+    turtle.ontimer(show_turtles,500)
 
 
 
 
+#fonctions 
+turtle.tracer(0)
 
 
+setup_turtles()
 setup_score_turtle()
+hide_turtles()
+show_turtles()
 
+
+
+turtle.tracer(1)
+gameBoard_setup()
 turtle.mainloop()
