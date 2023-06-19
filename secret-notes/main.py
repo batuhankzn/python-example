9599d4d2e4bf1ce9c354ef0bdebfd99c5d950ea1
Batuhan Kazan <batuhankzn61@gmail.com>
2023-06-20 01:33:01 +0300

    Update main.py

--- secret-notes/main.py
+++ secret-notes/main.py
@@ -1,15 +1,40 @@
 from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+#from cryptography.fernet import Fernet
+
 
 #UI
 screen = Tk()
 screen.title("Secret Notes")
 screen.minsize(500, 600)
-screen.config(padx=75, pady=75)
+screen.config(padx=50, pady=50)
 FONT = ("Verdana",20,"normal")
 
-photo = PhotoImage(file="topsecret.jpg")
-photo_label = Label(image=photo)
-photo_label.pack()
+#top secret logo
+image1 = Image.open("topsecret.png")
+topsecret = ImageTk.PhotoImage(image1)
+label1 = Label(image=topsecret)
+label1.image = topsecret
+label1.config(width=1000,height=150)
+label1.pack()
+
+
+def save_and_encrypt():
+    title = entry_title.get()
+    message = text_message.get("1.0", END)
+    master_secret = entry_key.get()
+
+    if len(title) == 0 or len(message) == 0 or len(master_secret) == 0:
+        messagebox.showwarning("Warning","Please enter all info")
+    else:
+        #encryp
+
+        with open("secret.txt","a") as datafile:
+            datafile.write(f"{text_message}")
+
+
+
 
 
 
@@ -31,11 +56,10 @@ key.pack()
 entry_key = Entry()
 entry_key.pack()
 
-save_button = Button(text="Save & Encrypt")
+save_button = Button(text="Save & Encrypt", command=save_and_encrypt)
 save_button.pack()
 
 decrypt_button = Button(text="Decrypt")
 decrypt_button.pack()
-
 
 screen.mainloop()
